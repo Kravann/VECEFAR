cf0a5634a69b05f4565f03bd2b42b97840405dce
ferlemos <ferlemos1993@gmail.com>
2023-02-13 22:55:15 -0300

    alta_lote UPDATE querys

--- alta_lote.php
+++ alta_lote.php
@@ -1,28 +1,36 @@
 <?php
-$id=$_GET['id'];
-$fec_in=$_GET['fec_in'];
-$ven=$_GET['venc'];
-$cant=$_GET['cant'];
-$prod=$_GET['producto'];
-$emp=$_SESSION['id'];
-$total=0;
-require "conect.php";
+    $id=$_GET['id'];
+    $fec_in=$_GET['fec_in'];
+    $ven=$_GET['venc'];
+    $cant=$_GET['cant'];
+    $prod=$_GET['producto'];
+    $emp=$_SESSION['id'];
 
-$r="INSERT INTO lotes values('$id','$fec_in','$ven','$cant','$prod','$emp')";
+    require "conect.php";
+
+    $r="INSERT INTO lotes values('$id','$fec_in','$ven','$cant','$prod','$emp') && UPDATE productos SET prod_stock_actual=$cant WHERE prod_id=$prod";
+
+    $resulset=mysqli_query($conn,$r);
+
+    $b="SELECT*FROM productos WHERE prod_id='$prod'";
+
+    $n=mysqli_query($conn,$b);
+
+    while($fila=mysqli_fetch_assoc($n)){
+        $a=$fila['prod_stock_actual'];
+    }
+    $a=$a+$cant;
+
+    $res="UPDATE productos SET prod_stock_actual=$a WHERE prod_id=$prod";
+    $resul=mysqli_query($conn,$res);
 
 
-$resulset=mysqli_query($conn,$r);
+    if(mysqli_affected_rows($conn)>0){
 
-
-if(mysqli_affected_rows($conn)>0){
-
-    echo "El lote se cargo exitosamente";
-    
+        echo "El lote se cargo exitosamente";
     }
     else{
-    echo "No se pudo cargar el lote";
+        echo "No se pudo cargar el lote";
     }
-
-
 
 ?>
