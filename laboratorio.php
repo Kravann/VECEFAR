fd67397bf314f1b5e87e7b08e46c9560078ceb55
Gonzalo <99339459+madeforyalo@users.noreply.github.com>
2023-02-27 12:33:56 -0300

    se cambiaron cositas
    -agregue fontawesome para iconos.
    -agregue header.
    -cambie el estilo del menu.
    -cerrar sesion pasa directo a index.
    - cositas varias

--- laboratorio.php
+++ laboratorio.php
@@ -1,16 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+session_start();
+if(isset($_SESSION['id']) && $_SESSION['tipoUsuario']==1){
+//todo ok
+
+}
+else{
+    echo"Pagina Prohibida. Inicie Sesion";
+    exit();
+}
+
+?>
+<?php include "header.php"; ?>
+
+<main class="d-flex flex-nowrap">
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
+      <span class="fs-4"><img src="imagenes/vecefar.png" alt="" style="width: 240px;"></span>
+
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="producto.php" class="nav-link text-white" aria-current="page">
+        <i class="fa-solid fa-table-list" width="16" height="16"></i>
+          Nuevo Producto
+        </a>
+      </li>
+	  <li>
+        <a href="droga.php" class="nav-link text-white">
+        <i class="fa-solid fa-capsules" width="16" height="16"></i>
+          Nueva Droga
+        </a>
+      </li>
+      <li>
+        <a href="lote.php" class="nav-link text-white">
+        <i class="fa-solid fa-box" width="16" height="16"></i>
+          Nuevo Lote
+        </a>
+      </li>
+	  <li>
+        <a href="laboratorio.php" class="nav-link text-white active">
+        <i class="fa-solid fa-industry" width="16" height="16"></i>
+          Nuevo laboratorio
+        </a>
+      </li>
+      <li>
+        <a href="nuevoemp.php" class="nav-link text-white">
+            <i class="fa-solid fa-user-plus" width="16" height="16"></i>
+          Nuevo Empleado
+        </a>
+      </li>
+    </ul>
+    <hr>
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="imagenes/v.png" alt="" width="32" height="32" class="rounded-circle me-2">
+        <strong>Admin</strong>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+        <li><a class="dropdown-item" href="#">Mi perfil</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="logout.php">Cerrar Sesión</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="b-example-divider b-example-vr"></div>
+
+</main>
+
+
     <form>
      ID Laboratorio<input type=number name=id required><br><br>
      Nombre<input type=text name=lab required><br><br>
      <input type=submit name=alta_lab>
     </form>
+
+    
+<script src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://getbootstrap.com/docs/5.3/examples/sidebars/sidebars.js"></script>
+
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
 session_destroy();
-echo "<h1>sesion cerrada</h1>";
-echo "<br><br><a href=index.php><button>Iniciar sesion</button></a>"
+Header("location: index.php");
 ?>
